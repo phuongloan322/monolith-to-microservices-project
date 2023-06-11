7a8f808849abdb49187da59a88b698cf67557a3f
Trần Thị Phương Loan <tranthiphuongloan@YuYuu.local>
2023-06-11 18:22:00 +0700

    t8

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1af4ad36b0ab467ba24e8c46881954c-836455168.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
